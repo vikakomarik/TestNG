54c2dfdf1daa6043775398a62d8c80a8534d91f1
Viktoriia Komarova <vikamail2011@gmail.com>
2023-04-26 19:12:39 +0200

    Fixed the input data

--- src/test/java/org/example/Calculation.java
+++ src/test/java/org/example/Calculation.java
@@ -87,7 +87,7 @@ public class Calculation {
     @DataProvider(name = "additionDataProvider")
     public Object[][] additionDataProvider() {
         return new Object[][] {
-                {1, 2, 3},
+                {1, 2, 0},
                 {-1, 1, 0},
                 {0, 0, 0}
         };
@@ -96,8 +96,8 @@ public class Calculation {
     @DataProvider(name = "subtractionDataProvider")
     public Object[][] subtractionDataProvider() {
         return new Object[][] {
-                {1, 2, -1},
-                {5, 3, 2},
+                {1, 2, 0},
+                {5, 3, 0},
                 {0, 0, 0}
         };
     }
@@ -105,8 +105,8 @@ public class Calculation {
     @DataProvider(name = "multiplicationDataProvider")
     public Object[][] multiplicationDataProvider() {
         return new Object[][] {
-                {2, 3, 6},
-                {4, -5, -20},
+                {2, 3, 0},
+                {4, -5, 0},
                 {0, 1, 0}
         };
     }
@@ -114,8 +114,8 @@ public class Calculation {
     @DataProvider(name = "divisionDataProvider")
     public Object[][] divisionDataProvider() {
         return new Object[][] {
-                {6, 3, 2},
-                {10, 5, 2},
+                {6, 3, 0},
+                {10, 5, 0},
                 {0, 1, 0}
         };
     }
